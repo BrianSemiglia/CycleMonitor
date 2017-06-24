f0511887d11b00ef7b4adcbc5a41c06b4c9dae02
Brian Semiglia <brian.semiglia@gmail.com>
2017-06-24 16:51:33 -0400

    Initial Commit

---
+++ CycleMonitor/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CycleMonitor
+//
+//  Created by Brian Semiglia on 6/24/17.
+//  Copyright © 2017 Brian Semiglia. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ CycleMonitor/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CycleMonitor
+//
+//  Created by Brian Semiglia on 6/24/17.
+//  Copyright © 2017 Brian Semiglia. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
