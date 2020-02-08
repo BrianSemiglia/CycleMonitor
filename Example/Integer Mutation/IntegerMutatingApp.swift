31cee7848ef12b5081913778a68895c81aaccf26
Brian Semiglia <brian.semiglia@gmail.com>
2020-02-08 03:34:16 -0500

    Broadcasts source code of for literal state.

--- Example/Integer Mutation/IntegerMutatingApp.swift
+++ Example/Integer Mutation/IntegerMutatingApp.swift
@@ -995,8 +995,8 @@ extension Observable {
                                     action: "\($0.event)",
                                     id: ""
                                 ),
-                                effect: "\($0.new)",
-                                context: "\($0.old.model)",
+                                effect: sourceCode($0.new),
+                                context: sourceCode($0.old.model),
                                 isApproved: false
                             )
                         )
@@ -1035,8 +1035,8 @@ extension Observable {
                                     action: "\($0.event)",
                                     id: ""
                                 ),
-                                effect: "\($0.new)",
-                                context: "\($0.old.model)",
+                                effect: sourceCode($0.new),
+                                context: sourceCode($0.old.model),
                                 isApproved: false
                             )
                         )
@@ -1109,4 +1109,41 @@ extension Moment.Driver {
         new.id = id
         return new
     }
+}
+
+func sourceCode(_ instance: Any, inset: String = "") -> String {
+    let mirror = Mirror(reflecting: instance)
+
+    var result = ""
+
+    result += String(reflecting: mirror.subjectType.self) + "(\n"
+    mirror.children.enumerated().forEach { index, tuple in
+        guard let label = tuple.label else { return }
+        let possibleComma = (mirror.children.count == index + 1 ? "" : ",")
+        result += inset + "\t"
+        switch Mirror(reflecting: tuple.value).displayStyle {
+        case .enum:
+            result += "\(label): " + "." + "\(tuple.value)" + possibleComma + "\n"
+        case .optional:
+            result += "\(label): " + "\(tuple.value)" + possibleComma + "\n"
+        case .struct:
+            result += "\(label): " + sourceCode(tuple.value, inset: inset + "\t") + possibleComma + "\n"
+        case _:
+            if type(of: tuple.value) == String.self {
+                result += "\(label): " + "\"\(tuple.value)\"" + possibleComma + "\n"
+            } else if type(of: tuple.value) == Bool.self || tuple.value as? Int != nil {
+                result += "\(label): " + "\(tuple.value)" + possibleComma + "\n"
+            } else {
+                result += "\(label): "
+                    + ".init"
+                    + "("
+                    + "\(tuple.value)"
+                    + possibleComma
+                    + "\n"
+            }
+        }
+    }
+    result += inset + ")"
+
+    return result
 }
