cc0789e42597feebc6b8a80b20d49d40cc4bfac1
Brian Semiglia <brian.semiglia@gmail.com>
2020-02-15 23:22:39 -0500

    Fixes compilation issues.

--- Example/Integer Mutation/MutatingLens+Multipeer.swift
+++ Example/Integer Mutation/MutatingLens+Multipeer.swift
@@ -11,49 +11,10 @@ import RxSwift
 import Cycle
 
 extension MutatingLens {
-
-    func multipeered<T>(
-        reducer: @escaping (T, Moment.Frame) -> T = { t, m in t }
-    ) -> MutatingLens<A, (B, MultipeerJSON), [Observable<Meta<T>>], Tag>
-    where A == Observable<Meta<T>>, C == [Labeled<Observable<Meta<T>>>] {
-        
-        let moments = Observable
-            .merge(self.set.map { $0.value }.tagged())
-            .map { ($0.tag, $0.1.summary) }
-            .map { states -> Moment in
-                Moment(
-                    drivers: NonEmptyArray(
-                        possible: self.set.map { $0.label }.enumerated().map { x in
-                            Moment.Driver(
-                                label: x.element,
-                                action: x.offset == states.0 ? states.1.cause.action : "", //
-                                id: String(x.offset)
-                            )
-                        }
-                    )!,
-                    frame: states.1.setting(
-                        cause: states.1.cause.setting(
-                            id: String(states.0)
-                        )
-                    )
-                )
-            }
-        
-            return MutatingLens<A, (B, MultipeerJSON), [Observable<Meta<T>>], Tag>(
-                value: value,
-                get: { values in (
-                    self.get,
-                    MultipeerJSON().rendering(moments) { multipeer, state in
-                        multipeer.render(state.coerced() as [AnyHashable: Any])
-                    }
-                )},
-                set: { _, _ in self.set.map { $0.value } }
-            )
-    }
     
-    func multipeered<T>() -> MutatingLens<A, (B, MultipeerJSON), [Observable<Meta<T>>], Tag>
+    func multipeered<T>() -> MutatingLens<A, (B, MultipeerJSON), [Observable<Meta<T>>]>
     where A == Observable<Meta<T>>, C == ([Observable<Meta<T>>], Observable<Moment>) {
-        MutatingLens<A, (B, MultipeerJSON), [Observable<Meta<T>>], Tag>(
+        MutatingLens<A, (B, MultipeerJSON), [Observable<Meta<T>>]>(
             value: value,
             get: { values in (
                 self.get,
@@ -65,9 +26,9 @@ extension MutatingLens {
         )
     }
     
-    func multipeered<T>() -> MutatingLens<A, (B, MultipeerJSON), C, Tag>
+    func multipeered<T>() -> MutatingLens<A, (B, MultipeerJSON), C>
     where A == Observable<Meta<T>>, C == ([Observable<Meta<T>>], Observable<Moment>) {
-        MutatingLens<A, (B, MultipeerJSON), C, Tag>(
+        MutatingLens<A, (B, MultipeerJSON), C>(
             value: value,
             get: { values in (
                 self.get,
@@ -80,10 +41,10 @@ extension MutatingLens {
     }
     
     func bugReported<T>(when: @escaping (T) -> Bool = { _ in false })
-        -> MutatingLens<A, (B, BugReporter), [Observable<Meta<T>>], Tag>
+        -> MutatingLens<A, (B, BugReporter), [Observable<Meta<T>>]>
         where A == Observable<Meta<T>>, C == ([Observable<Meta<T>>], Observable<Moment>)
     {
-        MutatingLens<A, (B, BugReporter), [Observable<Meta<T>>], Tag>(
+        MutatingLens<A, (B, BugReporter), [Observable<Meta<T>>]>(
             value: value,
             get: { values in (
                 self.get,
@@ -119,11 +80,9 @@ extension MutatingLens {
         )
     }
     
-    func momented<T>(
-        reducer: @escaping (T, Moment.Frame) -> T = { t, m in t }
-    ) -> MutatingLens<A, B, ([Observable<Meta<T>>], Observable<Moment>), Tag>
+    func momented<T>() -> MutatingLens<A, B, ([Observable<Meta<T>>], Observable<Moment>)>
     where A == Observable<Meta<T>>, C == [Labeled<Observable<Meta<T>>>] {
-        MutatingLens<A, B, ([Observable<Meta<T>>], Observable<Moment>), Tag>(
+        MutatingLens<A, B, ([Observable<Meta<T>>], Observable<Moment>)>(
             value: value,
             get: { _ in self.get },
             set: { _, _ in (
